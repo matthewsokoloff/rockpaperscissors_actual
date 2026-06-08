144b3e8d59131414e6dd81d47403a8a59b739d9e
Matthew Sokoloff <sokoloff.matthew27@gmail.com>
2026-06-07 22:33:27 -0400

    make pseudocode for rps

---
+++ script.js
@@ -0,0 +1,11 @@
+/* pseudocode for rock paper scissors
+for the computer: generate a random int between 1 and 3
+1 = rock, 2 = paper, 3 = scissors
+then ask the user for input of rock or paper or scissors (ignore case)
+and ensure their input is valid (if not, ask til they give valid input)
+then compare computer choice v user choice
+if they're the same, tie
+rock beats scissors, scissors beats paper, paper beats rock
+increment player or computer score upon a win
+after 5 rounds, compare the scores and display the final score + results
+*/
